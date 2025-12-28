1472cfad3e2e792fb89aee1ae536a15e3110497b
vinay <vinayvgupta15@gmail.com>
2025-12-29 00:32:47 +0530

    Update app.js logic

--- app.js
+++ app.js
@@ -5,13 +5,10 @@ const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
-// Middleware to parse JSON requests
 app.use(express.json());
 
-// Register routes
 app.use('/', userRoutes);
 
-// Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
